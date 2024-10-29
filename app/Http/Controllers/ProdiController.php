723024ffe8f51ec80482369b9075255e1007d7c7
dewaabdillah <dewaabdillahgans@gmail.com>
2024-10-29 22:04:39 +0700

    +FUNGSI EDIT & DELETE

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -35,4 +35,30 @@ public function save(Request $request)
 
     return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
 }
+public function edit($id)
+{
+    $prodi = Prodi::findOrFail($id);
+    return view('prodi.edit', compact('prodi'));
+}
+
+public function update(Request $request, $id)
+{
+    $request->validate([
+        'nama' => 'required'
+    ]);
+
+    $prodi = Prodi::findOrFail($id);
+    $prodi->update([
+        'nama' => $request->nama
+    ]);
+
+    return redirect()->route('prodi')->with('success', 'Program Studi berhasil diupdated');
+}
+public function delete($id)
+{
+    $prodi = Prodi::findOrFail($id);
+    $prodi->delete();
+
+    return redirect()->route('prodi')->with('success', 'Data Program Studi berhasil dihapus');
+}
 }
