d5cca9574fc1760ab551f5bbec8edce817bfb4b3
dewaabdillah <dewaabdillahgans@gmail.com>
2024-10-29 21:41:25 +0700

    +fungsi add

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -8,8 +8,31 @@ use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
+    // public function index()
+    // {
+    //     return view('prodi.index');
+    // }
     public function index()
-    {
-        return view('prodi.index');
-    }
+{
+    $prodis = Prodi::orderBy('id', 'desc')->get();
+    return view('prodi.index', compact('prodis'));
+}
+
+public function create()
+{
+    return view('prodi.create');
+}
+
+public function save(Request $request)
+{
+    $request->validate([
+        'nama' => 'required'
+    ]);
+
+    Prodi::create([
+        'nama' => $request->nama
+    ]);
+
+    return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
+}
 }
